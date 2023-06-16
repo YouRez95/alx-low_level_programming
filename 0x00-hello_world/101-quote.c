2eacb1fdb07b3325640d0322160c641e2d18b505
youness elalouani <younesselalouani1995@gmail.com>
2023-06-15 19:58:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,8 @@
  */
 int main(void)
 {
-	fprintf(
-			stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19u";
+
+	write(2, str, strnlen(str, sizeofstr));
+	return (0);
 }
