eacc85123d4f0c33d8e9a4f4a72896f7d19dd841
youness elalouani <younesselalouani1995@gmail.com>
2023-06-17 16:45:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int first_digit, second_digit;
+
+	for (first_digit = 0 ; first_digit <= 9 ; first_digit++)
+	{
+		for (second_digit = 0 ; second_digit <= 9 ; second_digit++)
+		{
+			if (first_digit == second_digit || first_digit > second_digit)
+			{
+				continue;
+			}
+			putchar('0' + first_digit);
+			putchar('0' + second_digit);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
