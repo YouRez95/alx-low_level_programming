fd7661ff7efdcd934154b20039d75930b9a9877f
youness elalouani <younesselalouani1995@gmail.com>
2023-06-20 15:57:23 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,19 @@
 
 /**
  * _islower - return 1 if c is lowercase else return 0
+ *
+ * @c: letter c
+ * Return: 1 if c is lowercase else return 1
  */
 
 int _islower(int c)
 {
 	if (islower(c))
 	{
-		return (1);	
-	} 
+		return (1);
+	}
 	else
 	{
 		return (0);
-	}	
+	}
 }
