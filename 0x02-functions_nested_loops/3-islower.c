2f38d7116358368634d52b0edad1ba868726d4b1
youness elalouani <younesselalouani1995@gmail.com>
2023-06-20 15:48:11 -0700

    3-islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _islower - return 1 if c is lowercase else return 0
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);	
+	} 
+	else
+	{
+		return (0);
+	}	
+}
