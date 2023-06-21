49b553e3902f6f758f7b8a3f6ab3ee839437b95e
youness elalouani <younesselalouani1995@gmail.com>
2023-06-20 18:24:05 -0700

    8-24_hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print  every minute
+ */
+
+void jack_bauer(void)
+{
+	char i, j, k, m;
+
+	for (m = '0' ; m <= '2' ; m++)
+	{
+		for (k = '0' ; k <= '9' ; k++)
+		{
+			for (j = '0' ; j <= '5' ; j++)
+			{	
+				for (i = '0' ; i <= '9' ; i++)
+				{
+					_putchar(m);
+					_putchar(k);
+					_putchar(':');
+					_putchar(j);
+					_putchar(i);
+					_putchar('\n');
+				}
+			}
+			if (m == '2' && k == '3')
+				break;
+		}
+	}
+}
