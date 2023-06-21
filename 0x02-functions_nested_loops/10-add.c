ff56dec8beda8a215b9c58107bd752901cc87f81
youness elalouani <younesselalouani1995@gmail.com>
2023-06-20 19:43:53 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * add - adddition of two numbers
+ *
+ * @a: param
+ * @b: param
+ * Reutrn: sum  of two numbers
+ */
+
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
