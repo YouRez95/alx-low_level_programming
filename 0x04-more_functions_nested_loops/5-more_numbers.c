6c360fbf8b8dfb8213ac0d85647acd5105280eae
YouRez95 <younesselalouani1995@gmail.com>
2023-06-23 02:32:54 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times numbers from 0 to 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	j = 0;
+	while (j < 10)
+	{
+		for (i = 0 ; i < 15 ; i++)
+		{
+			if (i > 9)
+			{
+				_putchar('0' + (i / 10));
+				_putchar('0' + (i % 10));
+			}
+			else
+			{
+				_putchar('0' + i);
+			}
+		}
+		_putchar('\n');
+		j++;
+	}
+}
