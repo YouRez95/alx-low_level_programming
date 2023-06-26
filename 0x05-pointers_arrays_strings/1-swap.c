c6e64e4310c780e42dcbdb955ceb1b9909e95858
YouRez95 <younesselalouani1995@gmail.com>
2023-06-26 18:30:22 +0100

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * swap_int - swipe two integers
+ *
+ * @a: integer 1
+ * @b: integer 2
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
