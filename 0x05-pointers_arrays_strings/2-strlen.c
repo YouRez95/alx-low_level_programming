cba7c89ba26b475d69e627dedf938ec00438fc5e
YouRez95 <younesselalouani1995@gmail.com>
2023-06-27 02:23:39 +0100

    2-strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - function return the lenght of string
+ *
+ * @s: param
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int length;
+
+	length = strlen(s);
+	return (length);
+}
