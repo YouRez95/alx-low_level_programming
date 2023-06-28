63b7c79a1aad46d3316cc3e12bfb63ff4152241e
YouRez95 <younesselalouani1995@gmail.com>
2023-06-28 02:26:57 +0100

    5-rev_string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,13 +12,16 @@ void rev_string(char *s)
 {
 	int i;
 	int length = strlen(s);
-	char reversed[10];
+	char *reversed[10];
 
 	for (i = 0 ; i < length ; i++)
 	{
-		reversed[i] = s[length - i - 1];
+		*reversed[i] = s[length - i - 1];
 	}
-	*s = reversed;
+	/*printf("the reverse s is : %s\n", reversed);*/
+	s = *reversed;
+	
+	/*s = reversed;*/
 	/*
 	*s =  here will be the string in reverse 
 	*/
