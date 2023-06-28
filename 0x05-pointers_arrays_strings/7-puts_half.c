06d4f2ddd9f61edb82ccc74e16eb862bde7032a3
YouRez95 <younesselalouani1995@gmail.com>
2023-06-28 01:35:42 +0100

    7-puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - print every other character of a string
+ *
+ * @str: input string
+ */
+void puts_half(char *str)
+{
+	unsigned long int i;
+
+	for (i = strlen(str)/2 ; i < strlen(str) ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
