3b84a47de1cf7374168d096d7910be4721d97738
YouRez95 <younesselalouani1995@gmail.com>
2023-07-07 12:56:28 +0100

    3-mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * @argc: counter
+ * @argv: array of pointers
+ *
+ * Return: 0 always Succes
+ */
+
+int main(int  argc, char **argv)
+{
+	long x1;
+	long x2;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (0);
+	}
+	x1 = strtol(argv[1], NULL, 10);
+	x2 = strtol(argv[2], NULL, 10);
+	printf("%ld\n", x1 * x2);
+	return (0);
+}
