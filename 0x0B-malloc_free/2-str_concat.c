0f8ac4c9240f7d58af456359fe6e071f100ca2b8
YouRez95 <younesselalouani1995@gmail.com>
2023-07-11 22:24:40 +0100

    str-conctenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *result;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	result = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 
