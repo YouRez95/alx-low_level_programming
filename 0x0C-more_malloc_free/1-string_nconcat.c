92a57bd2d4475d9ca342df585e197961b49af9fc
YouRez95 <younesselalouani1995@gmail.com>
2023-07-13 16:50:30 +0100

    string-nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int y;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 	if (n >= strlen(s2))
 		y = strlen(s1) + strlen(s2);
 	else
