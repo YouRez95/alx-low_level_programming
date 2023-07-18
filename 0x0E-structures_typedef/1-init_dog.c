e7475c135a1e18be2f54906f102a1d893a3e1e12
YouRez95 <younesselalouani1995@gmail.com>
2023-07-18 02:13:50 +0100

    init_dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * init_dog - struct dog
+ *
+ * @d: pointer to the dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 /**
  * struct dog - list of dogs
  * @name: name of the dog
