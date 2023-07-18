a1e004d2c71660a67006648fdc51e17ebd6218ff
YouRez95 <younesselalouani1995@gmail.com>
2023-07-18 01:45:36 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - list of dogs
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
