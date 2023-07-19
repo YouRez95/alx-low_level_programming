318f6bd996fa4e428ec41a87add77accf965ff8a
YouRez95 <younesselalouani1995@gmail.com>
2023-07-19 23:21:43 +0100

    get_op_function

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -22,9 +22,9 @@ int (*get_op_func(char *s))(int a, int b)
 	};
 	int i = 0;
 
-	while (ops[i].op)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(ops[i].op, s))
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
