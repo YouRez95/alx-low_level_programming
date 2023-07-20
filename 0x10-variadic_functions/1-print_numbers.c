0f68e448784ee7d9154bcdc228be4056f6ef2841
YouRez95 <younesselalouani1995@gmail.com>
2023-07-20 20:59:34 +0100

    print_numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list pargs;
 
+	if (n == 0)
+		return;
 	va_start(pargs, n);
 	for (i = 0; i < n ; i++)
 	{
