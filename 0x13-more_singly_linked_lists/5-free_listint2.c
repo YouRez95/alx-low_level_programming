ffce90944be36922335f7dcfd8d3a5fce22a67ae
YouRez95 <younesselalouani1995@gmail.com>
2023-07-31 17:59:09 +0100

    free 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,11 +13,11 @@ void free_listint2(listint_t **head)
 	listint_t *ptr, *tmp;
 
 	ptr = *head;
+	*head = NULL;
 	while (ptr)
 	{
 		tmp = ptr;
 		ptr = ptr->next;
 		free(tmp);
 	}
-	*head = NULL;
 }
