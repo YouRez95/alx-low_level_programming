cb52aa6989125c9023a3f151a487ba7cde16f628
YouRez95 <younesselalouani1995@gmail.com>
2023-07-31 17:51:26 +0100

    free 2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - free all lists
+ *
+ * @head: first node
+ *
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr, *tmp;
+
+	ptr = *head;
+	while (ptr)
+	{
+		tmp = ptr;
+		ptr = ptr->next;
+		free(tmp);
+	}
+	*head = NULL;
+}
