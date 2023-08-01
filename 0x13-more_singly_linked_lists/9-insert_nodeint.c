820dc430875d7aa7d297d27be732152c51606b37
YouRez95 <younesselalouani1995@gmail.com>
2023-08-01 17:12:10 +0100

    insert node at index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -31,7 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		i++;
 	}
-	if (temp == NULL)
+	if (tmp == NULL)
 		return (NULL);
 
 	newNode->next = tmp->next;
