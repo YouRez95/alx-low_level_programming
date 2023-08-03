078a4c5b2c30cb72e8a93a7dbd6ff9ad084ccd98
YouRez95 <younesselalouani1995@gmail.com>
2023-08-04 00:26:27 +0100

    get bit

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * get_bit - get bir by index;
+ *
+ * @n: n
+ * @index: index
+ *
+ * Return: 1 or 0
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int j;
+
+	if (n == 0)
+		return (0);
+	j = n >> index;
+	if (j & 1)
+		return (1);
+	else
+		return (0);
+}
