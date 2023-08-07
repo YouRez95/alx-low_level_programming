9bb3e9250211090cedf222f567448d25cbdf2e7a
YouRez95 <younesselalouani1995@gmail.com>
2023-08-07 23:59:17 +0100

    open-create-write

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+/**
+ * create_file - creating a file with permission -rw----- and write inside it
+ *
+ * @filename: filename
+ * @text_content: text want to write
+ *
+ * Return: 1 success -1 failed
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (-1);
+	return (1);
+}
