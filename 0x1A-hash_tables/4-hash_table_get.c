3eabe86976032ae60d5ca03941f77fd7f4e6f9fc
YouRez95 <younesselalouani1995@gmail.com>
2023-10-06 22:09:45 +0100

    get_value_from_table

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,10 +13,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
+	hash_node_t *item;
 
 	index = key_index((const unsigned char *) key, ht->size);
 	if (ht->array[index] == NULL)
 		return (NULL);
 
-	return (ht->array[index]->value);
+	item = ht->array[index];
+	while (item != NULL)
+	{
+		if (strcmp(item->key, key) == 0)
+		{
+			return (item->value);
+		}
+		item = item->next;
+	}
+	return (NULL);
 }
