46db88d6728da2996710449d0eda08c4dd3a7a6c
Youness Elalouani <younesselalouani1995@gmail.com>
2024-01-19 14:02:17 +0100

    fix jump search

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -65,7 +65,7 @@ int jump_search(int *array, size_t size, int value)
 			return (i);
 		}
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (i + jump > size)
+		if (i + jump >= size)
 		{
 			printf("Value found between indexes [%lu] and [%lu]\n", i, i + jump);
 			while (i < size)
